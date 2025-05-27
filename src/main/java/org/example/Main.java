ec7fd27796037af58fcd93a948fcc6e51e2ab702
DAfanadorC <afanadordiego22@gmail.com>
2025-05-27 16:33:52 -0500

    Agregado comentario en feature/comentario2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    // Funcionalidad 1: Comentario agregado por la rama feature/comentario1
+
+    // Funcionalidad 2: Comentario agregado por la rama feature/comentario2
+
 }
